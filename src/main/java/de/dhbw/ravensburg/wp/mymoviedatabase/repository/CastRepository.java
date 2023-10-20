7f4326c41964f0b424873e6bbbd026b4a84e82b2
Hechtei <hechtei0@gmail.com>
2023-10-20 11:59:32 +0200

    bla

--- src/main/java/de/dhbw/ravensburg/wp/mymoviedatabase/repository/CastRepository.java
+++
@@ -1,7 +0,0 @@
-package de.dhbw.ravensburg.wp.mymoviedatabase.repository;
-
-import de.dhbw.ravensburg.wp.mymoviedatabase.model.Cast;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CastRepository extends JpaRepository<Cast, Long> {
-}
